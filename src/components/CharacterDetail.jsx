f6c6cb0936870fd8aa5a7665f791980eea980847
Stefan <fusion44@laptop-antergos.fritz.box>
2017-11-26 17:35:36 +0100

    feat: implement delete character UI

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -14,7 +14,11 @@ import IconButton from "material-ui/IconButton"
 import Typography from "material-ui/Typography"
 import FavoriteIcon from "material-ui-icons/Favorite"
 import ShareIcon from "material-ui-icons/Share"
+import CloseIcon from "material-ui-icons/Close"
 import Link from "material-ui-icons/Link"
+import DeleteForever from "material-ui-icons/DeleteForever"
+import { GAME_CHARS_QUERY } from "./CharacterList"
+import Snackbar from "material-ui/Snackbar"
 
 const styles = theme => ({
   root: {
@@ -50,7 +54,52 @@ export const GAME_CHAR_QUERY = gql`
   }
 `
 
+export const DELETE_GAME_CHAR_QUERY = gql`
+  mutation deleteGameCharacter($charID: ID!) {
+    removeCharacter(id: $charID) {
+      op
+      count
+    }
+  }
+`
+
 class CharacterDetail extends Component {
+  state = {
+    snackBarOpen: false
+  }
+
+  onDeleteCharacter(event) {
+    const { characterId } = this.props.match.params
+    this.props
+      .deleteGameCharacter({
+        variables: { charID: characterId },
+        update: (store, { data }) => {
+          // Remove character from store, if the server deleted one
+          if (data.removeCharacter.count > 0) {
+            const gcQuery = store.readQuery({ query: GAME_CHARS_QUERY })
+            gcQuery.gameCharacters = gcQuery.gameCharacters.filter(char => {
+              return characterId === char.id ? null : char
+            })
+            store.writeQuery({ query: GAME_CHARS_QUERY, data: gcQuery })
+          }
+        }
+      })
+      .then(({ data }) => {
+        data.removeCharacter.count > 0
+          ? this.props.history.push("/")
+          : this.setState({ snackBarOpen: true })
+      })
+      .catch(err => console.log("Error: ", err)) // Show a error message to the user
+  }
+
+  handleRequestClose(event, reason) {
+    if (reason === "clickaway") {
+      return
+    }
+
+    this.setState({ snackBarOpen: false })
+  }
+
   render() {
     const { classes } = this.props
     const { loading } = this.props.data
@@ -80,6 +129,12 @@ class CharacterDetail extends Component {
               <IconButton aria-label="Share">
                 <ShareIcon />
               </IconButton>
+              <IconButton
+                onClick={this.onDeleteCharacter.bind(this)}
+                aria-label="Delete Forever"
+              >
+                <DeleteForever />
+              </IconButton>
               <div className={classes.flexGrow} />
               <IconButton
                 href={this.props.charWiki}
@@ -90,16 +145,34 @@ class CharacterDetail extends Component {
               </IconButton>
             </CardActions>
           </Card>
+          <Snackbar
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left"
+            }}
+            open={this.state.snackBarOpen}
+            autoHideDuration={6000}
+            onRequestClose={this.handleRequestClose.bind(this)}
+            SnackbarContentProps={{
+              "aria-describedby": "message-id"
+            }}
+            message={<span id="message-id">Error during delete</span>}
+            action={[
+              <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                className={classes.close}
+                onClick={this.handleRequestClose.bind(this)}
+              >
+                <CloseIcon />
+              </IconButton>
+            ]}
+          />
         </div>
       )
   }
 }
-
-let withQuery = graphql(GAME_CHAR_QUERY, {
-  options: props => ({
-    variables: { charID: props.match.params.characterId }
-  })
-})
 
 const mapStateToProps = ({ auth }, ownProps) => {
   if (ownProps.data.gameCharacter) {
@@ -123,6 +196,17 @@ const mapStateToProps = ({ auth }, ownProps) => {
   }
 }
 
-export default withRouter(
-  withStyles(styles)(withQuery(connect(mapStateToProps)(CharacterDetail)))
-)
+let withPrevious = connect(mapStateToProps)(CharacterDetail)
+
+withPrevious = graphql(GAME_CHAR_QUERY, {
+  options: props => ({
+    variables: { charID: props.match.params.characterId }
+  })
+})(withPrevious)
+
+withPrevious = graphql(DELETE_GAME_CHAR_QUERY, {
+  name: "deleteGameCharacter"
+})(withPrevious)
+
+withPrevious = withStyles(styles)(withPrevious)
+export default withRouter(withPrevious)
